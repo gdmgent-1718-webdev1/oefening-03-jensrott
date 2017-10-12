a6826d65d50e139674ca2b9b6f6cd706c88cec31
jensrott <jensrott@student.arteveldehs.be>
2017-10-12 17:47:54 +0200

    push

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$x = 10;
+$y = 7;
+//$z = $x + $y
+
+// Optellen
+echo $y . " + " . $x . " = " . ($y + $x) . "<br>";
+
+// is hetzelfde als hierboven
+//echo "${y} + ${x} = ${z}";
+
+// Aftrekken
+echo $y . " - " . $x . " = " . ($y - $x) . "<br>";
+
+// Vermenigvuldigen
+echo $y . " * " . $x . " = " . ($y * $x) . "<br>";
+
+// Delen
+echo $y . " / " . $x . " = " . ($y / $x) . "<br>";
+
+// Rest
+echo $y . " % " . $x . " = " . ($y % $x) . "<br>";
+
+?>
